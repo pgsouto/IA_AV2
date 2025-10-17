f25453d0f005d59939ba9dadd6629e67f63dd535
Pedro Santos <pedroguilherme.pessoal@gmail.com>
2025-10-17 16:52:15 -0300

    feat(adaline): modelo adaline completo + testes iniciais

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import seaborn as sns
 import cv2
 
 from models.perceptron import Perceptron
+from models.adaline import Adaline
 
 # ===============================
 # Funções auxiliares
@@ -54,9 +55,11 @@ plt.show()
 #--------------------------------------
 
 R = 500
-accuracies = []
+accuracies_perceptron = []
+accuracies_adaline = []
 
 perceptron = Perceptron(eta=0.1, max_epochs=100)
+adaline = Adaline(eta=0.01, max_epochs=200, eps=1e-6)
 
 for montecarlo_round in range(R):
     #Eambaralhar dados
@@ -64,6 +67,7 @@ for montecarlo_round in range(R):
     X_permuted = X_normalized[permuted_indexes]
     y_permuted = classes[permuted_indexes]
 
+    #Divisão em dados de treino e de teste
     split = int(0.8*len(X_permuted))
 
     X_train = X_permuted[:split]
@@ -72,27 +76,52 @@ for montecarlo_round in range(R):
     y_train = y_permuted[:split]
     y_test = y_permuted[split:]
 
+    #Treinamento do perceptron simples
     perceptron.fit(X_train, y_train)
 
-    y_pred = perceptron.predict(X_test)
-    acc = np.mean(y_pred == y_test)
+    #Teste do percptron simples
+    y_pred_perceptron = perceptron.predict(X_test)
+    acc_perceptron = np.mean(y_pred_perceptron == y_test)
 
-    accuracies.append(acc)
+    accuracies_perceptron.append(acc_perceptron)
+
+    #Treinamento do adaline
+    adaline.fit(X_train, y_train)
+
+    #teste do adaline
+    y_pred_adaline = adaline.predict(X_test)
+    acc_adaline = np.mean(y_pred_adaline == y_test)
+
+    accuracies_adaline.append(acc_adaline)
 
     if montecarlo_round == 0:
-        cm = matriz_confusao(y_test, y_pred)
+        cm_perceptron = matriz_confusao(y_test, y_pred_perceptron)
         plt.figure(figsize=(5,4))
-        sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
+        sns.heatmap(cm_perceptron, annot=True, fmt='d', cmap='Blues')
         plt.xlabel("Previsto")
         plt.ylabel("Real")
-        plt.title(f"Matriz de confusão - rodada {montecarlo_round+1}")
+        plt.title(f"Matriz de confusão (Perceptron) - rodada {montecarlo_round+1}")
+        plt.show()
+
+        cm_adaline = matriz_confusao(y_test, y_pred_adaline)
+        plt.figure(figsize=(5,4))
+        sns.heatmap(cm_adaline, annot=True, fmt='d', cmap='Blues')
+        plt.xlabel("Previsto")
+        plt.ylabel("Real")
+        plt.title(f"Matriz de confusão Adaline - rodada {montecarlo_round+1}")
         plt.show()
 # Estatísticas da acurácia
-print("=== Estatísticas da acurácia ===")
-print(f"Média: {np.mean(accuracies):.3f}")
-print(f"Desvio padrão: {np.std(accuracies):.3f}")
-print(f"Maior valor: {np.max(accuracies):.3f}")
-print(f"Menor valor: {np.min(accuracies):.3f}")
+print("=== Estatísticas da acurácia: Perceptron ===")
+print(f"Média: {np.mean(accuracies_perceptron):.3f}")
+print(f"Desvio padrão: {np.std(accuracies_perceptron):.3f}")
+print(f"Maior valor: {np.max(accuracies_perceptron):.3f}")
+print(f"Menor valor: {np.min(accuracies_perceptron):.3f}")
+
+print("=== Estatísticas da acurácia Adaline ===")
+print(f"Média: {np.mean(accuracies_adaline):.3f}")
+print(f"Desvio padrão: {np.std(accuracies_adaline):.3f}")
+print(f"Maior valor: {np.max(accuracies_adaline):.3f}")
+print(f"Menor valor: {np.min(accuracies_adaline):.3f}")
 
 # Curva de aprendizado da última rodada
 plt.figure()
@@ -100,4 +129,11 @@ plt.plot(range(1, len(perceptron.errors_by_epoch)+1), perceptron.errors_by_epoch
 plt.xlabel("Épocas")
 plt.ylabel("Erro total")
 plt.title("Curva de aprendizado - última rodada")
+plt.show()
+
+plt.figure()
+plt.plot(range(1, len(adaline.eqm_list)+1), adaline.eqm_list, marker='o')
+plt.xlabel("Épocas")
+plt.ylabel("EQM")
+plt.title("Curva de aprendizado Adaline - última rodada")
 plt.show()
